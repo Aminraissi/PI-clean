35423eb301513d1e10b6930b81849a12917dc399
Amine Raissi <116490546+Aminraissi@users.noreply.github.com>
2026-05-12 17:37:14 +0100

    Update plantDiseaseApiUrl for production environment

--- AgricultureFront/src/environments/environment.prod.ts
+++ AgricultureFront/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: '/api',
-  plantDiseaseApiUrl: 'https://plant-disease-api--rxnj5y5.politesky-86cbe2fa.francecentral.azurecontainerapps.io',
+  plantDiseaseApiUrl: 'https://plant-disease-api--qh6ebaf.victoriousmoss-f7c060b9.francecentral.azurecontainerapps.io',
 };
 
